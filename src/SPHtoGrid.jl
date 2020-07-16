9c09b6da0a565d72568f972f06925d5b78ae5601
LudwigBoess <lboess@usm.lmu.de>
2020-07-16 10:29:30 +0200

    Filter for relevant particles before mapping

--- src/SPHtoGrid.jl
+++ src/SPHtoGrid.jl
@@ -13,6 +13,7 @@ module SPHtoGrid
            kernel_value_2D, kernel_value_3D,
            mappingParameters,                         # parameters for SPH mapping
            sphMapping,                                # main function for mapping 
+           filter_particles_in_image,                 # helper function to preselect particles
            read_smac1_binary_info,
            read_smac1_binary_image,
            write_smac1_par,
@@ -74,6 +75,38 @@ module SPHtoGrid
         return x, mappingParameters(center=cen, x_lim=xlim, y_lim=ylim, z_lim=zlim, Npixels=maximum(par.Npixels))
     end
 
+    """
+        filter_particles_in_image(x, hsml, param::mappingParameters)
+
+    Checks if a particle is contained in the image and returns an array of Bool.
+    """
+    function filter_particles_in_image(x, hsml, param::mappingParameters)
+
+        p_in_image = falses(length(hsml))
+        in_image   = false
+
+        minCoords  = [param.x_lim[1], param.y_lim[1], param.z_lim[1]]
+        maxCoords  = [param.x_lim[2], param.y_lim[2], param.z_lim[2]]
+
+        @inbounds for p = 1:length(hsml)
+
+            @inbounds for dim = 1:3
+
+                in_image = check_in_image(Float64(x[p, dim]), Float64(hsml[p]),
+                                        minCoords[dim], maxCoords[dim])
+
+                # exit the loop if the particle is not in the image frame
+                if !in_image
+                    break
+                end
+            end
+
+            p_in_image[p] = in_image
+
+        end
+
+        return p_in_image
+    end
 
     """
         sphMapping(Pos, HSML, M, ρ, Bin_Quant;
@@ -82,6 +115,7 @@ module SPHtoGrid
                 show_progress::Bool=true,
                 conserve_quantities::Bool=true,
                 parallel::Bool=true,
+                filter_particles::Bool=true,
                 dimensions::Int=2])
 
     Maps the data in `Bin_Quant` to a grid. Parameters of mapping are supplied in
@@ -95,8 +129,9 @@ module SPHtoGrid
     - `Bin_Quant`: Array with particle quantity to be mapped.
     - `kernel::SPHKernel`: Kernel object to be used.
     - `show_progress::Bool=true`: Show progress bar.
-    - `parallel::Bool=true`: Run on multiple processors.
     - `conserve_quantities::Bool=true`: If quantities should be conserved while mapping, like in Smac (Dolag et. al. 2005).
+    - `parallel::Bool=true`: Run on multiple processors.
+    - `filter_particles::Bool=true`: Find the particles that are actually contained in the image.
     - `dimensions::Int=2`: Number of mapping dimensions (2 = to grid, 3 = to cube).
     """
     function sphMapping(Pos, HSML, M, ρ, Bin_Quant;
@@ -105,15 +140,30 @@ module SPHtoGrid
                         show_progress::Bool=true,
                         conserve_quantities::Bool=false,
                         parallel::Bool=false,
+                        filter_particles::Bool=true,
                         dimensions::Int=2)
 
+        #filter particles if they are contained in the image
+        if filter_particles
+            p_in_image = filter_particles_in_image(Pos, HSML, param)
+        else
+            p_in_image = trues(length(Bin_Quant))
+        end
+
+        # allocate reduced arrays
+        x     = Pos[p_in_image, :]
+        hsml  = HSML[p_in_image]
+        m     = M[p_in_image]
+        rho   = ρ[p_in_image]
+        bin_q = Bin_Quant[p_in_image]
+
         # First check if all particles are in a positive region and shift them if they are not
-        x, par = check_center_and_move_particles(Pos, param)
+        x, par = check_center_and_move_particles(x, param)
 
         if (dimensions == 2)
 
             if !parallel
-                return sphMapping_2D(x, HSML, M, ρ, Bin_Quant;
+                return sphMapping_2D(x, hsml, m, rho, bin_q;
                                     param=par, kernel=kernel,
                                     conserve_quantities=conserve_quantities,
                                     show_progress=show_progress)
@@ -129,9 +179,9 @@ module SPHtoGrid
 
                 # start remote processes
                 for (i, id) in enumerate(workers())
-                    futures[i] = @spawnat id sphMapping_2D(x[batch[i],:], HSML[batch[i]],
-                                                        M[batch[i]], ρ[batch[i]],
-                                                        Bin_Quant[batch[i]];
+                    futures[i] = @spawnat id sphMapping_2D(x[batch[i],:], hsml[batch[i]],
+                                                        m[batch[i]], rho[batch[i]],
+                                                        bin_q[batch[i]];
                                                         param=par, kernel=kernel,
                                                         conserve_quantities=conserve_quantities,
                                                         show_progress=false)
@@ -143,7 +193,7 @@ module SPHtoGrid
 
         elseif (dimensions == 3 )
             if !parallel
-                return sphMapping_3D(x, HSML, M, ρ, Bin_Quant;
+                return sphMapping_3D(x, hsml, m, rho, bin_q;
                                     param=par, kernel=kernel,
                                     show_progress=show_progress)
             else
@@ -158,10 +208,10 @@ module SPHtoGrid
 
                 # start remote processes
                 for (i, id) in enumerate(workers())
-                    futures[i] = @spawnat id sphMapping_3D(x[batch[i],:], HSML[batch[i]],
-                                                        M[batch[i]], ρ[batch[i]],
-                                                        Bin_Quant[batch[i]];
-                                                        par=param, kernel=kernel,
+                    futures[i] = @spawnat id sphMapping_3D(x[batch[i],:], hsml[batch[i]],
+                                                        m[batch[i]], rho[batch[i]],
+                                                        bin_q[batch[i]];
+                                                        param=par, kernel=kernel,
                                                         conserve_quantities=conserve_quantities,
                                                         show_progress=false)
                 end
