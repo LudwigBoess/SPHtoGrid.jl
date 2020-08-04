e301257079b258c67cfc2d83962dc3ec1a42f498
Ludwig Boess <di67meg@c2paplogin.cos.lrz.de>
2020-08-04 12:05:38 +0200

    reduce 2D-array images

--- src/reduce_image.jl
+++ src/reduce_image.jl
@@ -21,12 +21,45 @@ Unflattens an image array to a 2D array.
     return im_plot
 end
 
+
+@inline @fastmath function reduce_image_2D( image::Array{<:Real},
+                                            x_pixels::Int64, y_pixels::Int64)
+
+
+    im_plot = zeros(x_pixels, y_pixels)
+    k = 1
+    @inbounds for i = 1:x_pixels, j = 1:y_pixels
+        im_plot[i,j] = image[k,1]
+        if image[k,2] > 0.0
+            im_plot[i, j] /= image[k,2]
+        end
+        k += 1
+    end
+    return im_plot
+end
+
 """
     function reduce_image_3D( image::Array{<:Real}, w_image::Array{<:Real},
                                             x_pixels::Int64, y_pixels::Int64, z_pixels::Int64)
 
 Unflattens an image array to a 3D array.
 """
+@inline @fastmath function reduce_image_3D( image::Array{<:Real},
+                                            x_pixels::Int64, y_pixels::Int64, z_pixels::Int64)
+
+
+    im_plot = zeros(x_pixels, y_pixels, z_pixels)
+    m = 1
+    @inbounds for i = 1:x_pixels, j = 1:y_pixels, k = 1:z_pixels
+        im_plot[i,j,k] = image[m,1]
+        if image[m,1] > 0.0
+            im_plot[i, j, k] /= image[m,2]
+        end
+        m += 1
+    end
+    return im_plot
+end
+
 @inline @fastmath function reduce_image_3D( image::Array{<:Real}, w_image::Array{<:Real},
                                          x_pixels::Int64, y_pixels::Int64, z_pixels::Int64)
 
@@ -42,7 +75,6 @@ Unflattens an image array to a 3D array.
     end
     return im_plot
 end
-
 
 """
     function reduce_futures(fut::Array{<:Tuple})
