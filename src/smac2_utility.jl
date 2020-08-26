ad944d950fbc82e78e443d470b00dbd0d85de245
LudwigBoess <lboess@usm.lmu.de>
2020-08-26 16:11:07 +0200

    generalized input parameters to avoid 32bit test crash

--- src/smac2_utility.jl
+++ src/smac2_utility.jl
@@ -6,10 +6,10 @@ Writes a P-Smac2 parameter file. Not all relevant parameters implemented yet!
 """
 function write_smac2_par(x, y, z,
                          euler_angle_0, euler_angle_1, euler_angle_2,
-                         xy_size, z_depth, xy_pix::Int64,
+                         xy_size, z_depth, xy_pix::Integer,
                          input_file, output_file, path,
-                         effect_module::Int64=0, effect_flag::Int64=0,
-                         CR_pmin::Float64=10.0, CR_pmax::Float64=1.e7)
+                         effect_module::Integer=0, effect_flag::Integer=0,
+                         CR_pmin::Real=10.0, CR_pmax::Real=1.e7)
 
     open(path * "smac2.par", "w") do f
         write(f,
