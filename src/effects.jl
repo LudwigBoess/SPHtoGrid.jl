ddc4b0cf6e931a51025311da813234e3a85aab2f
Ludwig Boess <di67meg@c2paplogin.cos.lrz.de>
2020-09-07 17:53:10 +0200

    cleanup and comments

--- src/effects.jl
+++ src/effects.jl
@@ -3,6 +3,9 @@
                 Mass::Real=1.989e43, Length::Real=3.085678e21)
 
 Computes the 2D density.
+
+# Returns
+2D surface density in untis of [g/cm^2].
 """
 function density_2D(rho::Real, pixelSideLength::Real, 
                     Mass::Real=1.989e43, Length::Real=3.085678e21)
@@ -17,7 +20,10 @@ end
 
 X-Ray emission of a particle with number density `n_cm3` in ``1/cm^3`` and temperature `T` in ``eV``.
 `Emin` and `Emax` give the minimum and maximum energy of the oberservation.
-`xH` gives the hydrogen fraction used in the simulation.  
+`xH` gives the hydrogen fraction used in the simulation.
+
+# Returns
+X-Ray surface brightness contribution in units of [erg/cm^2/s/Hz].
 """
 function x_ray_emission(n_cm3::Real, T_eV::Real; 
                         Emin::Real=5.e4, Emax::Real=1.e10, 
@@ -38,32 +44,58 @@ Computes the temperature of the CMB at redshift `z`.
 """
 Tcmb(z::Real) = ( 2.728 * ( 1.0 + z ) )
 
-k_B = 1.38066e-16
-c   = 2.9979e10
-σ_T = 6.65245e-25
-m_e = 9.10953e-28
 
 """
-    kSzPrefac  = -1.0 * σ_T / c
+    kSzPrefac(ν::Real, z::Real, DI_over_I::Bool)
 
 Prefactor for the kinetic Sunyaev-Zel'dovich effect.
 """
-global const kSzPrefac  = -1.0 * σ_T / c
+function kSzPrefac(ν::Real, z::Real, DI_over_I::Bool)
+
+    c   = 2.9979e10
+    σ_T = 6.65245e-25
+    
+    kSzPrefac  = -1.0 * σ_T / c
+
+    if DI_over_I
+        h   = 6.6261e-27
+        k_B = 1.38066e-16
+        x   = h * ν / ( k_B * Tcmb(z) )
+
+        kSzPrefac *= exp(x) - 1 / (x * exp(x))
+    end
+
+    return kSzPrefac
+end
+
 
 """
-    yPrefac    = σ_T * k_B / (m_e * c * c)
+    yPrefac()
 
-Precator for the Compton-Y parameter.
+Prefactor for the Compton-Y parameter.
 """
-global const yPrefac    = σ_T * k_B / (m_e * c * c)
+function yPrefac()   
+
+    k_B = 1.38066e-16
+    c   = 2.9979e10
+    σ_T = 6.65245e-25
+    m_e = 9.10953e-28
+
+    return σ_T * k_B / (m_e * c * c)
+end
 
 """
-    kinetic_SZ(n_cm3::Real, vel_y_cgs::Real)
+    kinetic_SZ(n_cm3::Real, vel_y_cgs::Real, 
+                    ν::Real=1.e9, z::Real=0.0; 
+                    DI_over_I::Bool=false)
 
 Computes the kinetic Sunyaev-Zel'dovich effect from electron density `n_cm3` and velocity in y-direction to the projection plane in cgs units `vel_y_cgs`.
+If `DI_over_I` is set to `true` you also need to provide an observation frequency `ν` and redshift `z`.
 """
-function kinetic_SZ(n_cm3::Real, vel_y_cgs::Real)
-    return kSzPrefac * n_cm3 * vel_y_cgs
+function kinetic_SZ(n_cm3::Real, vel_y_cgs::Real, 
+                    ν::Real=1.e9, z::Real=0.0; 
+                    DI_over_I::Bool=false)
+    return kSzPrefac(ν, z, DI_over_I) * n_cm3 * vel_y_cgs
 end
 
 """
@@ -72,7 +104,7 @@ end
 Computes the Compton-Y parameter from electron density `n_cm3` and temperature `T` in Kelvin at redshift `z`.
 """
 function comptonY(n_cm3::Real, T_K::Real, z::Real)
-    return yPrefac * n_cm3 * ( T_K - Tcmb(z) )
+    return yPrefac() * n_cm3 * ( T_K - Tcmb(z) )
 end
 
 """
@@ -80,18 +112,27 @@ end
 
 Computes the prefactor for the thermal Sunyaev-Zel'dovich effect.
 """
-function tSzPrefac(ν::Real, z::Real)
+function tSzPrefac(ν::Real, z::Real, DI_over_I::Bool)
     h   = 6.6261e-27
     k_B = 1.38066e-16
     x   = h * ν / ( k_B * Tcmb(z) )
-    return (x * (exp(x) + 1.0) / (exp(x) - 1.0) - 4.0)
+    tSzPrefac = (x * (exp(x) + 1.0) / (exp(x) - 1.0) - 4.0)
+
+    if DI_over_I
+        tSzPrefac *= exp(x) - 1 / (x * exp(x))
+    end
+
+    return tSzPrefac
+
 end
 
 """
     thermal_SZ(n_cm3::Real, T::Real, z::Real=0.0, ν::Real=1.44e9)
 
-Computes the thermal Sunyaev-Zel'dovich effect for electron density `n_cm3` and temperature `T` at redshift `z` and observer frequency `ν`.
+Computes the thermal Sunyaev-Zel'dovich effect for electron density `n_cm3` and temperature `T` in Kelvin at redshift `z` and observer frequency `ν`.
 """
-function thermal_SZ(n_cm3::Real, T::Real, z::Real=0.0, ν::Real=1.44e9)
-    return tSzPrefac(ν, z) * comptonY(n_cm3, T, z)
+function thermal_SZ(n_cm3::Real, T::Real, 
+                    z::Real=0.0, ν::Real=1.44e9; 
+                    DI_over_I::Bool=false)
+    return tSzPrefac(ν, z, DI_over_I) * comptonY(n_cm3, T, z)
 end
