2c5fecfe1f7defca13ad94687c519d34b7f00445
Ludwig Boess <di67meg@c2paplogin.cos.lrz.de>
2020-11-16 16:58:57 +0100

    fixed computation of x-ray emission

--- src/effects/x_ray.jl
+++ src/effects/x_ray.jl
@@ -1,21 +1,22 @@
 """
-    x_ray_emission(n_cm3::Real, T_eV::Real; 
+    x_ray_emission(n_cm3::Real, T_K::Real; 
                    Emin::Real=5.e4, Emax::Real=1.e10, 
                    xH::Real=0.76)
 
-X-Ray emission of a particle with number density `n_cm3` in ``1/cm^3`` and temperature `T` in ``eV``.
+X-Ray emission of a particle with number density `n_cm3` in ``1/cm^3`` and temperature `T` in ``K``.
 `Emin` and `Emax` give the minimum and maximum energy of the oberservation.
 `xH` gives the hydrogen fraction used in the simulation.
 
 # Returns
 X-Ray surface brightness contribution in units of [erg/cm^2/s/Hz].
 """
-function x_ray_emission(n_cm3::Real, T_eV::Real; 
+function x_ray_emission(n_cm3::Real, T_K::Real; 
                         Emin::Real=5.e4, Emax::Real=1.e10, 
                         xH::Real=0.76)
 
     prefac = 4.0 * 2.42e-24 / (1 + xH)
-    k_B = 1.38066e-16
+    k_B    = 1.38066e-16
+    T_eV   = T_K / 1.60218e-12
 
     return prefac * sqrt( k_B * T_eV * 1.e-3 ) * n_cm3^2 *
             ( exp( -Emin / (k_B * T_eV) ) - exp( -Emax / (k_B * T_eV) ) )
