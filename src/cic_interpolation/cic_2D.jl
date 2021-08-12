7d98c6e9cad85dd7fc7360d116dd2c296f5f116e
LudwigBoess <lboess@usm.lmu.de>
2021-08-12 14:00:07 +0200

    moved cic 2D and 3D mapping functions to seperate files

---
+++ src/cic_interpolation/cic_2D.jl
@@ -0,0 +1,206 @@
+"""
+    get_d_hsml( dx::Real, dy::Real,
+                   hsml_inv::Real )
+
+Computes the distance in 2D to the pixel center in units of the kernel support.
+"""
+@inline function get_d_hsml(dx::T, dy::T, hsml_inv::T) where T
+    √( dx*dx + dy*dy ) * hsml_inv
+end
+
+
+
+"""
+    function calculate_index(i::Integer, j::Integer, x_pixels::Integer)
+
+Calculates the index of a flattened 2D image array.
+"""
+function calculate_index(i::T, j::T, x_pixels::T) where T
+    return floor(T, i * x_pixels + j ) + 1
+end
+
+
+"""
+    function calculate_weights_2D(  wk::Array{<:Real,1}, 
+                                    iMin::Integer, iMax::Integer, 
+                                    jMin::Integer, jMax::Integer,
+                                    x::Real, y::Real, hsml::Real, hsml_inv::Real,
+                                    kernel::SPHKernel,
+                                    x_pixels::Integer )
+
+Calculates the kernel- and geometric weights of the pixels a particle contributes to.
+"""
+@fastmath function calculate_weights( wk::Vector{Float64}, 
+                                      iMin::Integer, iMax::Integer, 
+                                      jMin::Integer, jMax::Integer,
+                                      x::Real, y::Real, hsml::Real, hsml_inv::Real,
+                                      kernel::SPHKernel,
+                                      x_pixels::Integer )
+
+    is_undersampled = false
+
+    if hsml <= 1.0
+        is_undersampled = true
+    end
+
+    distr_weight = 0.0
+    n_distr_pix  = 0
+
+
+    @inbounds for i = iMin:iMax
+        x_dist, dx = get_x_dx(x, hsml, i)
+
+        for j = jMin:jMax
+            y_dist, dy = get_x_dx(y, hsml, j)
+
+            # index in flattened 2D array
+            idx = calculate_index(i, j, x_pixels)
+
+            dxdy = dx * dy
+
+            if is_undersampled
+
+                wk[idx]       = dxdy
+                distr_weight += dxdy
+                n_distr_pix  += 1
+
+            else # is_undersampled
+
+                u = get_d_hsml(x_dist, y_dist, hsml_inv)
+
+                # check if inside the kernel
+                if u <= 1.0
+                    # evaluate kernel
+                    wk[idx]       = 𝒲₃(kernel, u, hsml_inv)
+                    wk[idx]      *= dxdy
+                    distr_weight += wk[idx]
+                    n_distr_pix  += 1
+                else
+                    wk[idx] = 0.0
+                end # u < 1.0
+
+            end # is_undersampled
+
+        end # j
+    end # i
+
+    return wk, n_distr_pix, distr_weight
+end
+
+"""
+    get_quantities_2D( pos, weight, hsml, 
+                       rho, m, len2pix::T) where T
+
+Helper function to convert quantities to pixel units and the correct data type.
+"""
+function get_quantities_2D( pos, weight, hsml, 
+                            rho, m, len2pix::T) where T
+        
+    hsml    *= T(len2pix)
+    hsml_inv = T(1.0)/hsml
+    area     = π * hsml^2
+
+    rho *= T(1.0/(len2pix*len2pix*len2pix))
+    dz   = m / rho / area
+
+    return T.(pos), T(weight), hsml, hsml_inv, area, dz
+end
+
+
+"""
+   sphMapping_2D( Pos, HSML, 
+                  M, Rho, 
+                  Bin_Q, Weights;
+                  param::mappingParameters, kernel::SPHKernel,
+                  show_progress::Bool=false )
+
+Underlying function to map SPH data to a 2D grid.
+"""
+function sphMapping_2D( Pos, HSML, 
+                        M, Rho, 
+                        Bin_Q, Weights;
+                        param::mappingParameters, kernel::SPHKernel,
+                        show_progress::Bool=false )
+
+    N = size(M,1)  # number of particles
+    
+    N_distr = param.Npixels[1] * param.Npixels[2]
+
+    image = zeros(Float64, N_distr, 2)
+
+    if param.periodic
+        k_start = 0
+    else
+        k_start = 7
+    end
+
+    # max number of pixels over which the particle can be distributed
+    N_distr = param.Npixels[2] * param.Npixels[1]
+
+    # allocate arrays for weights
+    wk = zeros(Float64, N_distr)
+
+
+    if show_progress
+        P = Progress(N)
+    end
+
+    # loop over all particles
+    @inbounds for p = 1:N
+
+        bin_q = Float64(Bin_Q[p])
+
+        if bin_q == 0.0
+            continue
+        end
+
+        _pos, weight, hsml, hsml_inv, area, dz = get_quantities_2D(Pos[:,p], Weights[p], HSML[p], Rho[p], M[p], param.len2pix)
+
+        for k = k_start:7
+
+            # simplify position quantities for performance
+            x, y, z, skip_k = get_xyz( _pos, HSML[p], k, param)
+
+            if skip_k
+                continue
+            end
+            
+            # calculate relevant pixel range
+            iMin, iMax = pix_index_min_max( x, hsml, param.Npixels[1] )
+            jMin, jMax = pix_index_min_max( y, hsml, param.Npixels[2] )
+
+            wk, n_distr_pix, distr_weight = calculate_weights(wk, iMin, iMax, jMin, jMax,
+                                                              x, y, hsml, hsml_inv, 
+                                                              kernel,
+                                                              param.Npixels[1])
+           
+            # skip if the particle is not contained in the image 
+            # ( should only happen with periodic boundary conditions )
+            if n_distr_pix == 0
+                continue
+            end
+
+            weight_per_pix  = n_distr_pix / distr_weight
+            kernel_norm     = area / n_distr_pix
+            area_norm       = kernel_norm * weight_per_pix * weight * dz
+
+            @inbounds for i = iMin:iMax, j = jMin:jMax
+                idx = calculate_index(i, j, param.Npixels[1])
+
+                image[idx,1], image[idx,2] = update_image( image[idx,1], image[idx,2], 
+                                                           wk[idx], bin_q, area_norm)
+                
+            end # i, j 
+
+        end # k
+
+
+         # update for ProgressMeter
+        if show_progress
+            next!(P)
+        end
+    end # p
+
+    return image
+
+end # function
---
+++ src/cic_interpolation/cic_shared.jl
@@ -0,0 +1,99 @@
+"""
+    function pix_index_min_max(x::T, hsml::T, n_pixels::Int64) where T
+
+Calculates the minimum and maximum pixel to which a particle contributes.
+"""
+function pix_index_min_max(x::T, hsml::T, n_pixels::Int64) where T
+
+    iMin = max(floor(Integer, x - hsml), 0)
+    iMax = min(floor(Integer, x + hsml), n_pixels-1)
+
+    return iMin, iMax
+end
+
+
+"""
+    function get_dxyz(x::Real, hsml::Real, i::Integer)
+
+Calculates the extent of the current particle size in units of pixels.
+"""
+function get_dxyz(x::Real, hsml::Real, i::Integer)
+    min(x + hsml, i + 1) - max(x - hsml, i)
+end
+
+
+"""
+    function get_x_dx(x, hsml, i)
+
+
+"""
+@inline function get_x_dx(x, hsml, i)
+
+    dx = get_dxyz(x, hsml, i)
+    x_dist = x - i - 0.5
+
+    return x_dist, dx
+end
+
+
+"""
+    function get_xyz( pos, hsml, k::Integer,
+                      par::mappingParameters)
+
+Calculates `x, y, z` position in units of pixels and performs periodic mapping, if required.
+"""
+function get_xyz( pos, hsml, k::Integer,
+                  par::mappingParameters)
+    
+    if par.periodic
+
+        # perform periodic mapping of particle
+        x, y, z = find_position_periodic(pos, k, par.boxsize)
+
+        # check if the particle is still contained in the image frame
+        # after the periodic mapping
+        if !particle_in_image(x, y, z, hsml, par.halfsize)
+
+            # if not return skip_particle as true
+            return x, y, z, true
+        end
+
+    else
+        # unpack array
+        x, y, z = pos
+    end
+
+    # convert from code units to pixels
+    x *= par.len2pix
+    y *= par.len2pix
+
+    x += 0.5 * par.Npixels[1]
+    y += 0.5 * par.Npixels[2]
+    
+    return x, y, z, false
+
+end
+
+
+
+"""
+    function update_image( image::Real, w_image::Real, 
+                           wk::Real, bin_q::Real, 
+                           geometry_norm::Real )
+
+Applies the different contributions to the image and the weight image.
+"""
+function update_image( image::Float64, w_image::Float64, 
+                        wk::Float64, bin_q::Float64, 
+                        geometry_norm::Float64 )
+
+    if wk > 0.0
+        pix_weight  = geometry_norm * wk
+        image      += bin_q * pix_weight
+        w_image    += pix_weight
+    end
+
+    return image, w_image
+end
+
+
