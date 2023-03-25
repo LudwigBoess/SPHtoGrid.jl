42178eb2ecac488ba7ef1ffcfb1972d712323432
Ludwig Böss <ludwig.boess@outlook.de>
2023-03-25 12:29:24 +0100

    move from hsml as absis for depth to rho. Calculate mass on grid and in particles.

--- src/healpix_interpolation/main.jl
+++ src/healpix_interpolation/main.jl
@@ -135,22 +135,6 @@ function find_in_shell(Δx::Vector{T}, radius_limits::Vector{T}) where T
 end
 
 
-"""
-    particle_area_and_depth(d, hsml, pix_radian)
-
-Computes the particle area and depth.
-Caution: This has to be represented as a cylinder instead of a sphere, which introduces an error by design.
-Also computes the pixel radius at the particle horizon.
-"""
-function particle_area_and_depth(hsml::T) where T
-
-    # general particle quantities
-    area = π * hsml^2
-    dz = 2*hsml
-
-    return area, dz
-end
-
 
 """
     contributing_pixels(pos, hsml, r)
@@ -221,13 +205,31 @@ function update_image!(allsky_map, weight_map, wk,
 
 end
 
+
+
+"""
+    particle_area_and_depth(d, hsml, pix_radian)
+
+Computes the particle area and depth.
+Caution: This has to be represented as a cylinder instead of a sphere, which introduces an error by design.
+Also computes the pixel radius at the particle horizon.
+"""
+function particle_area_and_depth(hsml::T, m::T, rho::T) where {T}
+
+    # general particle quantities
+    dz = 2hsml
+    area = (m / rho) / dz
+
+    return area, dz
+end
+
 """
     filter_sort_particles(Pos, Hsml, Bin_q, Weights, center, radius_limits)
 
 Filters the particles that are in the shell that should be mapped and sorts them by according to their position along the line of sight.
 Returns arrays with only relevant particles
 """
-function filter_sort_particles(Pos, Hsml, Bin_q, Weights, center, radius_limits)
+function filter_sort_particles(Pos, Hsml, M, Rho, Bin_q, Weights, center, radius_limits)
 
     # subtract center
     Pos .-= center
@@ -246,16 +248,20 @@ function filter_sort_particles(Pos, Hsml, Bin_q, Weights, center, radius_limits)
     hsml = Hsml[sorted[sel]]
     bin_q = Bin_q[sorted[sel]]
     weights = Weights[sorted[sel]]
+    rho = Rho[sorted[sel]]
+    m = M[sorted[sel]]
 
     # free memory allocated for full arrays
     Pos = nothing
     Hsml = nothing
     Bin_q = nothing
     Weights = nothing
+    M = nothing 
+    Rho = nothing
     GC.gc()
 
     # return relevant particle arrays
-    return pos, hsml, bin_q, weights
+    return pos, hsml, m, rho, bin_q, weights
 end
 
 
@@ -269,7 +275,7 @@ end
 
 Calculate an allsky map from SPH particles.
 """
-function healpix_map(Pos, Hsml, Bin_q, Weights;
+function healpix_map(Pos, Hsml, M, Rho, Bin_q, Weights;
     center::Vector{<:Real}=[0.0, 0.0, 0.0],
     Nside::Integer=1024,
     kernel::AbstractSPHKernel,
@@ -288,7 +294,7 @@ function healpix_map(Pos, Hsml, Bin_q, Weights;
     end
 
     # filter and sort the particles, return arrays with only relevant particles
-    pos, hsml, bin_q, weights = filter_sort_particles(Pos, Hsml, Bin_q, Weights, center, radius_limits)
+    pos, hsml, m, rho, bin_q, weights = filter_sort_particles(Pos, Hsml,  M, Rho, Bin_q, Weights, center, radius_limits)
 
     # Npart after filtering 
     Npart = length(hsml)
@@ -310,7 +316,11 @@ function healpix_map(Pos, Hsml, Bin_q, Weights;
     # storage array for kernel weights
     wk = Vector{Float64}(undef, length(allsky_map))
     # storage array for mapped area
-    A  = Vector{Float64}(undef, length(allsky_map))
+    A = Vector{Float64}(undef, length(allsky_map))
+
+    # storage for grid and particle masses 
+    grid_mass = 0.0
+    part_mass = 0.0
 
     # define progressmeter
     P = Progress(Npart)
@@ -341,18 +351,22 @@ function healpix_map(Pos, Hsml, Bin_q, Weights;
         pixidx = contributing_pixels(pos[:, ipart], hsml[ipart], Δx, res, allsky_map)
 
         # area of particle and length along line of sight
-        area, dz = particle_area_and_depth(hsml[ipart])
-        
+        area, dz = particle_area_and_depth(hsml[ipart], m[ipart], rho[ipart])
+
         # calculate kernel weights and mapped area
         wk, A, N, weight_per_pix = calculate_weights(wk, A, pos[:, ipart], hsml[ipart],
-                                                     Δx, res, pixidx, pix_radius, kernel)
+            Δx, res, pixidx, pix_radius, kernel)
 
         # update the actual images
         update_image!(allsky_map, weight_map, wk,
-                      pixidx,
-                      area, dz,
-                      A, N, weight_per_pix,
-                      weights[ipart], bin_q[ipart])
+            pixidx,
+            area, dz,
+            A, N, weight_per_pix,
+            weights[ipart], bin_q[ipart])
+
+        # store mass on grid and in particles 
+        grid_mass += rho[ipart] * sum(wk[1:length(pixidx)]) * sum(A[1:length(pixidx)]) * dz
+        part_mass += m[ipart]
 
         # update the progress meter
         update_progress!(P, min_worker, show_progress)
@@ -362,6 +376,10 @@ function healpix_map(Pos, Hsml, Bin_q, Weights;
         println()
         @info "Number of zero pixels in image: $(length(findall(iszero.(allsky_map))))"
         println()
+        @info "Mass conservation:"
+        @info "\tMass on grid:      $(grid_mass*1.e10) Msun"
+        @info "\tMass in particles: $(part_mass*1.e10) Msun"
+        @info "\tRel. Error:        $(abs(part_mass-grid_mass)/part_mass)"
     end
 
     return allsky_map, weight_map
